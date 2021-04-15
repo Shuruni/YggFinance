be8d5e9d98aed5cf486dbdb861fa6537a5346292
Shuruni <speedster708@gmail.com>
2021-04-15 18:29:11 -0500

    modified planningService to handle multipart/form-data

--- src/planningService/server.js
+++ src/planningService/server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // Constants
 const PORT = 8080;
@@ -9,9 +11,15 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.use(express.json()) // for parsing application/json
-app.post('/', function (req, res, next) {
-    console.log(req.body)
-    res.json(req.body)
+app.post('/', upload.single('file'), function (req, res, next) {
+    console.log(req.file, req.body);
+    res.json({
+      transactions: [
+        {merchant: "merchant1", amount: 7, date: "2020/07/23", isReconciled: true},
+        {merchant: "merchant2", amount: 42, date: "2021/01/17", isReconciled: true},
+        {merchant: "merchant3", amount: 69, date: "2021/04/15", isReconciled: false}
+      ]
+    });
   });
 
 // app.post('/', (req, res) => {
