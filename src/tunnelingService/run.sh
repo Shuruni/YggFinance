4c4490730ce31474b8112c9c63dd49e94c13b09e
Shuruni <speedster708@gmail.com>
2021-04-23 21:32:33 -0500

    enabled subdomain config in env-var in docker-compose.yml

--- src/tunnelingService/run.sh
+++ src/tunnelingService/run.sh
@@ -3,7 +3,7 @@
 echo "Running localtunnel startup bash script..."
 
 function localtunnel {
-lt --subdomain yggfinancedev --local-host reverse-proxy --port 80
+lt --subdomain $LT_SUBDOMAIN --local-host reverse-proxy --port 80
 }
 
 until localtunnel; do
