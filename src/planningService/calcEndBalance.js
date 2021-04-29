fcd46de64f6917798d4be1ec24fb785d68e14323
BlakeHudson <blakehudson615@gmail.com>
2021-04-29 09:35:19 -0500

    added comments and functionality to calcTimeFrame

--- src/planningService/calcEndBalance.js
+++ src/planningService/calcEndBalance.js
@@ -1,9 +1,9 @@
 import { request } from "express";
 
 /**
- * 
- * performs the calculation to determine the time ending amount of an investment after some time.
- * 
+ * Computes Future value, total contributions, and amout of interest earned for a given savings goal.
+ * Uses compound interest formula for calculations
+ *  
  * @param {{initialInvestment: number,avgRateOfReturn: number,monthlyContributions: number,planningMode: "boolean",timeFrame: number,savingsGoal: number}} requestBody 
  * @param {{endBalance: number,timeFrame: number,startingAmount: number,totalContributions: number,totalInterest: number}} responseBody 
  */
@@ -15,18 +15,20 @@ import { request } from "express";
     let timeFrame = requestBody.timeFrame;
     let savingsGoal = requestBody.savingsGoal;
     let planningMode = requestBody.planningMode;
-
-    //combines r/t for simplification, all calculations are monthly (12 periods)
+    //combines r/t for simpliying use in formulas, all calculations are monthly (12 periods)
     let rate = avgRate/12;  
-     // calcs future value without contributions
+   
+    // calcs future value without contributions
     let futureValue = initialAmount * (1 + (rate)) ** (timeFrame * 12);    
-     // calcs contributions plus return rate
+   
+    // calcs contributions with return rate compunding
     let contribWithInt = monthlyContributions * ((((1 + rate) ** (timeFrame * 12)) - 1)/rate); 
-    // total end balance plus contributions
+  
+    // Conpute outputs
     let endBalance = futureValue + contribWithInt;                           
-    // Inital investment plus monthly contributins for length of timeFrame
+   
     let totalContributed = initialAmount + (12 * timeFrame * monthlyContributions) 
-    // finds total iterest by subtracting principl and contributions    
+  
     let totalInterest = endBalance - totalContributed;   
 
 
