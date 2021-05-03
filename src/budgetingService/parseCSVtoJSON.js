f5e78880b6282cda8afacd085ee8443be95657bc
Shuruni <speedster708@gmail.com>
2021-05-03 00:31:58 -0500

    moved request parsing to parseRequest function

--- src/budgetingService/parseCSVtoJSON.js
+++ src/budgetingService/parseCSVtoJSON.js
@@ -7,20 +7,15 @@ import csv from "csv-parser";
  * function to handle the processing of the CSV file
  * 
  * @param {string} filePath the path of the csv file to parse
- * @param {string} hasHeaders is the first row of the csv header information? (true or false)
- * @param {string} merchantColumn which column letter contains the merchant information?
- * @param {string} amountColumn which column letter contains the amount information?
- * @param {string} dateColumn which column letter contains the date information?
+ * @param {number} hasHeaders is the first row of the csv header information?
+ * @param {number} merchantColumn which column index contains the merchant information?
+ * @param {number} amountColumn which column index contains the amount information?
+ * @param {number} dateColumn which column index contains the date information?
  * @returns {Promise} a Promise that resolves to a JSON Array of transaction objects
  */
 async function parseCSVtoJSON(filePath, hasHeaders, merchantColumn, amountColumn, dateColumn) {
   return new Promise ((resolve) => {
     let results = [];
-
-    hasHeaders = (String(hasHeaders).toLowerCase() == "true") ? 1 : 0;
-    merchantColumn = columnToIndex(merchantColumn);
-    amountColumn = columnToIndex(amountColumn);
-    dateColumn = columnToIndex(dateColumn);
 
     fs.createReadStream(filePath)
       .pipe(csv({ headers: false, skipLines: hasHeaders }))
@@ -43,18 +38,6 @@ async function parseCSVtoJSON(filePath, hasHeaders, merchantColumn, amountColumn
         resolve(transactions);
       });
   });
-}
-
-/**
-   * Takes in a String containing a single char representing a column on a csv file.
-   * The ASCII char code is the received and 97 is subtracted
-   * subtracting 97 will sync lowercase 'a' with 0
-   * @param {*String} column string containing single char representing a column on csv file
-   * @returns column index by number
-   */
- function columnToIndex(column) {
-  column = column.toLowerCase();
-  return column.charCodeAt(0) - 97;
 }
 
 export default parseCSVtoJSON;
--- src/budgetingService/parseRequest.js
+++ src/budgetingService/parseRequest.js
@@ -7,16 +7,29 @@ import parseCSVtoJSON from "./parseCSVtoJSON.js";
  * @returns {Promise} a Promise that resolves to a JSON Array of transaction objects
  */
 function parseRequest(req) {
+    /**
+     * Takes in a String containing a single char representing a column on a csv file.
+     * @param {*String} column string containing single char representing a column on csv file
+     * @returns column index by number
+     */
+    function columnToIndex(column) {
+        column = column.toLowerCase();
+        return column.charCodeAt(0) - 97;
+    }
+    
     // Retrieve the Headers from req.body
-    let hasHeaders = req.body.hasHeaders;
-    let merchantColumn = req.body.merchantColumn;
-    let amountColumn = req.body.amountColumn;
-    let dateColumn = req.body.dateColumn;
+    let hasHeaders = (String(req.body.hasHeaders).toLowerCase() == "true") ? 1 : 0;
+    let merchantColumn = columnToIndex(req.body.merchantColumn);
+    let amountColumn = columnToIndex(req.body.amountColumn);
+    let dateColumn = columnToIndex(req.body.dateColumn);
 
     // Retrieve the path of the csv file from the req.file fields
     let filePath = req.file.path;
 
     return parseCSVtoJSON(filePath, hasHeaders, merchantColumn, amountColumn, dateColumn);
 }
+
+
+ 
 
 export default parseRequest;
